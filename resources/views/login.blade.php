2abccbaeb82bf5ac598da0bae54bedf02d2255ae
JulieNobody <julie.nobody@gmail.com>
2020-09-23 14:35:37 +0200

    test4

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,10 @@
 @extends('Templates\template')
 
 @section('title')
-    BOSS MINAS
+    BOSS Julie
 @endsection
 
 @section('contenu')
-    <h1 id="titre">Formule 1</h1>
-    <h1 id="titre">Formule 2</h1>
+    <h1 id="titre">Formule 6</h1>
+    <h1 id="titre">Formule 99</h1>
 @endsection
