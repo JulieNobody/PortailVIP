805d05851c605bc460c7bcdd75088c333ee65ed0
JulieNobody <julie.nobody@gmail.com>
2020-09-23 14:23:40 +0200

    test3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenu')
-    <h1 id="titre">Modification de PHILIPPE !</h1>
-    <h1 id="titre">Bonjour les Pouloulous ! C'est moi !</h1>
+    <h1 id="titre">Modification de PHILIPPE ! GNAGNA GNA</h1>
+    <h1 id="titre">Bonjour les Pouloulous ! C'est moi la boss!</h1>
 @endsection
 
