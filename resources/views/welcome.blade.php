fca2fcd6bea5803031250dc4489931628d1cccdc
Philippe <philippe.thomas.iphone@gmail.com>
2020-09-23 14:48:47 +0200

    changement welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TITRE PHIL</title>
+        <title>NIKTARACE</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -130,6 +130,6 @@
                 </div>
             </div>
         </div>
-        <p>LOREM GNA GNA GNA</p>
+        <p>nikmouk nardine babek</p>
     </body>
 </html>
