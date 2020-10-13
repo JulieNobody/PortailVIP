c71ebb3a97c90c070234e415b7223cce6b4ffb46
Philippe <philippe.thomas.iphone@gmail.com>
2020-10-13 17:32:40 +0200

    js detail inter

--- resources/views/Interventions/detail_intervention.blade.php
+++ resources/views/Interventions/detail_intervention.blade.php
@@ -13,115 +13,126 @@ Intervention n°{{$intervention->NumInt}}
 <script type="text/javascript">
     $(document).ready(function(){
 
-        $('#monBouton').click(function(){
-
-            $('#coordonnees_client').toggle(200);
+        $('.detail-titre').click(function(){
+            $(this).nextAll('.aCacher').eq(0).toggle(200);
         });
-
-
 
     });
 </script>
+
+
+
     <h1 class="titrePopup">Détail intervention {{$intervention->NumInt}}</h1>
 
-    <div class="admin-crea-form">
+    <div class="detail">
 
-        <fieldset>
-            <legend>Coordonnées client</legend>
-            <button id="monBouton" type='button' >Click me</button>
-            <div id="coordonnees_client" style='display: none'>
+            <div class="detail-titre">
+                <h2>Coordonnées client</h2>
+            </div>
+
+            <div class="aCacher" id="coordonnees_client" style="display: none">
                 <div>
-                    <h2>Client Facturation</h2>
+                    <h3>Client Facturation</h3>
                     <ul>
-                        <li>Nom client : {{$intervention->NomFactCli}}</li>
-                        <li>Adresse : {{$intervention->AdFactCli}}</li>
-                        <li>Code Postal : {{$intervention->CPFactCli}}</li>
-                        <li>Ville : {{$intervention->VilleFactCli}}</li>
-                        <li>Interlocuteur :{{$intervention->InterlocFactCli}}</li>
-                        <li>Tel: {{$intervention->TelFactCli}}</li>
-                        <li>Fax: {{$intervention->FaxFactCli}}</li>
+                        <li><span>Nom client :</span> {{$intervention->NomFactCli}}</li>
+                        <li><span>Adresse :</span> {{$intervention->AdFactCli}}</li>
+                        <li><span>Code Postal :</span> {{$intervention->CPFactCli}}</li>
+                        <li><span>Ville :</span> {{$intervention->VilleFactCli}}</li>
+                        <li><span>Interlocuteur :</span> {{$intervention->InterlocFactCli}}</li>
+                        <li><span>Tel :</span> {{$intervention->TelFactCli}}</li>
+                        <li><span>Fax :</span> {{$intervention->FaxFactCli}}</li>
                     </ul>
                 </div>
                 <div>
-                    <h2>Client Livraison</h2>
+                    <h3>Client Livraison</h3>
                     <ul>
-                        <li>Nom client : {{$intervention->NomLivCli}}</li>
-                        <li>Adresse : {{$intervention->AdLivCli}}</li>
-                        <li>Code Postal : {{$intervention->CPLivCli}}</li>
-                        <li>Ville : {{$intervention->VilleLivCli}}</li>
-                        <li>Interlocuteur :{{$intervention->InterlocLivCli}}</li>
-                        <li>Tel: {{$intervention->TelLivCli}}</li>
-                        <li>Fax: {{$intervention->FaxLivCli}}</li>
+                        <li><span>Nom client :</span> {{$intervention->NomLivCli}}</li>
+                        <li><span>Adresse :</span> {{$intervention->AdLivCli}}</li>
+                        <li><span>Code Postal :</span> {{$intervention->CPLivCli}}</li>
+                        <li><span>Ville :</span> {{$intervention->VilleLivCli}}</li>
+                        <li><span>Interlocuteur :</span> {{$intervention->InterlocLivCli}}</li>
+                        <li><span>Tel :</span> {{$intervention->TelLivCli}}</li>
+                        <li><span>Fax :</span> {{$intervention->FaxLivCli}}</li>
                     </ul>
                 </div>
             </div>
-        </fieldset>
 
-        <fieldset>
-            <legend>Détail demande client</legend>
-            <ul>
-                <li>Référence client : {{$intervention->RefDossierCli}}</li>
-                <li>Marque : {{$intervention->Marque}}</li>
-                <li>Type Appareil : {{$intervention->TypeApp}}</li>
-                <li>Numéro de série : {{$intervention->NumSerie}}</li>
-                <li>Date début garantie : </li>
-                <li>Date fin garantie : {{$intervention->DateFinSG}}</li>
-                <li>Date enregistrement : {{$intervention->DateEnr->format('d-m-Y')}}</li>
-                <li>Problème rencontré : {{$intervention->ligneDetail->DesignArt}}</li>
-            </ul>
-        </fieldset>
 
-        <fieldset>
-            <legend>Détail visites</legend>
-            <ul>
-                <li>Date début intervention : {{$intervention->DateDebInterv}}</li>
-                <li>Date fin intervention : {{$intervention->DateFinInterv}}</li>
-            </ul>
-        </fieldset>
 
-        <fieldset>
-            <legend>Documents associés</legend>
-            <div class="wrapper">
-                <div class="table">
-                    <div class="row header">
-                        <div class="cell" data-title="NomDocument">
-                            Nom document
+            <div class="detail-titre">
+                <h2>Détail demande client</h2>
+            </div>
+            <div class="aCacher" id="detail_demande_client">
+                <ul>
+                    <li><span>Référence client :</span> {{$intervention->RefDossierCli}}</li>
+                    <li><span>Marque :</span> {{$intervention->Marque}}</li>
+                    <li><span>Type Appareil :</span> {{$intervention->TypeApp}}</li>
+                    <li><span>Numéro de série :</span> {{$intervention->NumSerie}}</li>
+                    <li><span>Date début garantie :</span> </li>
+                    <li><span>Date fin garantie :</span> {{$intervention->DateFinSG}}</li>
+                    <li><span>Date enregistrement :</span> {{$intervention->DateEnr->format('d-m-Y')}}</li>
+                    <li><span>Problème rencontré :</span> {{$intervention->ligneDetail->DesignArt}}</li>
+                </ul>
+            </div>
+
+
+            <div class="detail-titre">
+                <h2>Détail visites</h2>
+            </div>
+            <div class="aCacher" id="detail_visites">
+                <ul>
+                    <li><span>Date début intervention :</span> {{$intervention->DateDebInterv}}</li>
+                    <li><span>Date fin intervention :</span> {{$intervention->DateFinInterv}}</li>
+                </ul>
+            </div>
+
+
+            <div class="detail-titre">
+                <h2>Documents associés</h2>
+            </div>
+            <div class="aCacher" id="documents_associes">
+                <div class="wrapper">
+                    <div class="table">
+                        <div class="row header">
+                            <div class="cell" data-title="NomDocument">
+                                Nom document
+                            </div>
+                            <div class="cell" data-title="Date">
+                                Date
+                            </div>
+                            <div class="cell" data-title="Heure">
+                                Heure
+                            </div>
                         </div>
-                        <div class="cell" data-title="Date">
-                            Date
+
+                        {{-- @if(count($documents) == 0)
+                            </div>
+                                <p class="aucunResultat" >
+                                    Aucun document
+                                </p>
+                        @else --}}
+
+                        {{-- @foreach ($docuements as $d) --}}
+
+                        <div class="row">
+                            <div class="cell" data-title="NomDocument">
+                                <a href="">nom du doc</a>
+                            </div>
+                            <div class="cell" data-title="Date">
+                                date du doc
+                            </div>
+                            <div class="cell" data-title="Heure">
+                                heure du doc
+                            </div>
                         </div>
-                        <div class="cell" data-title="Heure">
-                            Heure
-                        </div>
+                        {{-- @endforeach
+
+                        @endif --}}
                     </div>
-
-                    {{-- @if(count($documents) == 0)
-                        </div>
-                            <p class="aucunResultat" >
-                                Aucun document
-                            </p>
-                    @else --}}
-
-                    {{-- @foreach ($docuements as $d) --}}
-
-                    <div class="row">
-                        <div class="cell" data-title="NomDocument">
-                            <a href="">nom du doc</a>
-                        </div>
-                        <div class="cell" data-title="Date">
-                            date du doc
-                        </div>
-                        <div class="cell" data-title="Heure">
-                            heure du doc
-                        </div>
-                    </div>
-                    {{-- @endforeach
-
-                    @endif --}}
                 </div>
             </div>
 
-        </fieldset>
+
 
     </div>
 
