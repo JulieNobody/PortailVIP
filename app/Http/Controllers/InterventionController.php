83730f45e7b641e63551e7a00e31bc6cb9b33967
Philippe <philippe.thomas.iphone@gmail.com>
2020-10-20 14:55:33 +0200

    intervupdate

--- app/Http/Controllers/InterventionController.php
+++ app/Http/Controllers/InterventionController.php
@@ -52,7 +52,7 @@ class InterventionController extends Controller
         //Récupération de toutes les interventions concernants l'utilisateur connecté
         $interventions = new Intervention;
 
-        if(auth()->user()->Acces != 111111111){
+        if(auth()->user()->Acces[8] != 1){
 
             $interventions = $interventions->where('NomCmdCli', '=', $username);
 
